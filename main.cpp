51985b8975793f5050cdbfe0c0c42cf150606667
ironlumps <ryan.kassab@att.net>
2018-09-13 20:27:00 +0000

    Added Comments

--- main.cpp
+++ main.cpp
@@ -4,13 +4,19 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+  // Initialize class object
   DNA dna;
+  // Checks to see if an argument is given
   if(argc > 1)
   {
+    //Gets filename from command line argument
     string fileName = argv[1];
+    // Opens input file stream reader
     ifstream checkFile(fileName);
+    // Checks if the file exists
     if(checkFile.good())
     {
+      // Calls upon the execute function to run the whole program
       dna.execute(fileName);
     }
     else
